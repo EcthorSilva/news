766fa2034da3db3802e919905a537fc8bb20d020
Ecthor Silva <EcthorSilva@outlook.com>
2024-05-29 15:52:50 -0300

    reorganização

--- main.js
+++ main.js
@@ -6,57 +6,57 @@ let contextMenu = null;
 let mainWindow = null;
 
 app.whenReady().then(() => {
-  tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
+   tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
 
-  tray.setToolTip('news-app');
+   tray.setToolTip('news-app');
 
-  contextMenu = Menu.buildFromTemplate([
-    { label: 'Abrir Janela', click: openMainWindow },
-    { label: 'Configurações', click: () => { console.log('Configurações clicado'); } },
-    { type: 'separator' },
-    { label: 'Sair', click: () => { app.quit(); } }
-  ]);
+   contextMenu = Menu.buildFromTemplate([
+      { label: 'Abrir Janela', click: openMainWindow },
+      { label: 'Configurações', click: () => { console.log('Configurações clicado'); } },
+      { type: 'separator' },
+      { label: 'Sair', click: () => { app.quit(); } }
+   ]);
 
-  tray.setContextMenu(contextMenu);
+   tray.setContextMenu(contextMenu);
 
-  tray.on('click', () => {
-    if (mainWindow) {
-      mainWindow.show();
-    } else {
-      createMainWindow();
-    }
-  });
+   tray.on('click', () => {
+      if (mainWindow) {
+         mainWindow.show();
+      } else {
+         createMainWindow();
+      }
+   });
 
-  tray.on('right-click', () => {
-    tray.popUpContextMenu(contextMenu);
-  });
+   tray.on('right-click', () => {
+      tray.popUpContextMenu(contextMenu);
+   });
 });
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    }
-  });
+   mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false,
+      }
+   });
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+   mainWindow.on('closed', () => {
+      mainWindow = null;
+   });
 }
 
 function openMainWindow() {
-  if (mainWindow) {
-    mainWindow.show();
-  } else {
-    createMainWindow();
-  }
+   if (mainWindow) {
+      mainWindow.show();
+   } else {
+      createMainWindow();
+   }
 }
 
 app.on('window-all-closed', () => {
-  app.quit();
+   app.quit();
 });
