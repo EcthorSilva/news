fc329d772a1a09cf98583ceeed2e678539097d6a
Ecthor Silva <EcthorSilva@outlook.com>
2024-05-30 22:55:30 -0300

    definindo algumas configurações do tray

--- main.js
+++ main.js
@@ -2,16 +2,60 @@ const { resolve } = require('path');
 const { app, Menu, Tray, BrowserWindow } = require('electron');
 
 let tray = null;
-let contextMenu = null;
 let mainWindow = null;
+
+function createMainWindow() {
+   mainWindow = new BrowserWindow({
+      width: 300, // Largura da janela
+      height: 600,
+      show: false, // Não mostrar imediatamente
+      frame: false, // Remover a barra de título
+      fullscreenable: false, // Não permitir tela cheia
+      resizable: false, // Não permitir redimensionamento
+      webPreferences: {
+         nodeIntegration: true,
+         contextIsolation: false,
+      }
+   });
+
+   const indexPath = resolve(__dirname, 'index.html');
+   mainWindow.loadFile(indexPath);
+
+   mainWindow.on('closed', () => {
+      mainWindow = null;
+   });
+
+   mainWindow.once('ready-to-show', () => {
+      mainWindow.show();
+   });
+
+   // Mostrar a janela acima do ícone do tray
+   const trayBounds = tray.getBounds();
+   const windowBounds = mainWindow.getBounds();
+   const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
+   const y = Math.round(trayBounds.y - windowBounds.height); // Posicionar acima do ícone do tray
+   mainWindow.setPosition(x, y, false);
+}
+
+function toggleMainWindow() {
+   if (mainWindow && mainWindow.isVisible()) {
+      mainWindow.hide();
+   } else {
+      if (!mainWindow) {
+         createMainWindow();
+      } else {
+         mainWindow.show();
+      }
+   }
+}
 
 app.whenReady().then(() => {
    tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
 
    tray.setToolTip('news-app');
 
-   contextMenu = Menu.buildFromTemplate([
-      { label: 'Abrir Janela', click: openMainWindow },
+   const contextMenu = Menu.buildFromTemplate([
+      { label: 'Abrir Janela', click: toggleMainWindow },
       { label: 'Configurações', click: () => { console.log('Configurações clicado'); } },
       { type: 'separator' },
       { label: 'Sair', click: () => { app.quit(); } }
@@ -20,42 +64,13 @@ app.whenReady().then(() => {
    tray.setContextMenu(contextMenu);
 
    tray.on('click', () => {
-      if (mainWindow) {
-         mainWindow.show();
-      } else {
-         createMainWindow();
-      }
+      toggleMainWindow();
    });
 
    tray.on('right-click', () => {
       tray.popUpContextMenu(contextMenu);
    });
 });
-
-function createMainWindow() {
-   mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-         nodeIntegration: true,
-         contextIsolation: false,
-      }
-   });
-
-   mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-   mainWindow.on('closed', () => {
-      mainWindow = null;
-   });
-}
-
-function openMainWindow() {
-   if (mainWindow) {
-      mainWindow.show();
-   } else {
-      createMainWindow();
-   }
-}
 
 app.on('window-all-closed', () => {
    app.quit();
